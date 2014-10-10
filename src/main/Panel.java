966bfa46d0e5b49e62c15937dd28f626b6214053
David Boyd <davidboyd96@gmail.com>
2014-10-10 21:55:55 +0100

    Panel

---
+++ src/main/Panel.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+
+
+	
+	public Panel()
+	{
+
+		setFocusable(true);
+	}
+
+}
