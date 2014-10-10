d0058991a185f82e38aa6178357c6cde03b83ddc
KB9 <kavanbickerstaff@googlemail.com>
2014-10-10 23:59:16 +0100

    Level updated
    Draws tiles now.

--- src/main/Level.java
+++ src/main/Level.java
@@ -1,17 +1,30 @@
 package main;
 
+import java.awt.Graphics2D;
+
 public class Level {
 	
 	private final int TILE_SIZE = 64;
 	
 	private Tile[][] mLevelTiles;
+	private int mLevelSize;
 
 	public Level(int levelSize) {
-		mLevelTiles = new Tile[levelSize][levelSize];
+		mLevelSize = levelSize;
+		mLevelTiles = new Tile[mLevelSize][mLevelSize];
 		
-		for(int column = 0; column < levelSize; column ++) {
-			for(int row = 0; row < levelSize; row ++) {
+		for(int column = 0; column < mLevelSize; column ++) {
+			for(int row = 0; row < mLevelSize; row ++) {
 				mLevelTiles[column][row] = new Tile(TILE_SIZE);
+			}
+		}
+	}
+	
+	public void draw(Graphics2D g2d) {
+		for(int column = 0; column < mLevelSize; column ++) {
+			for(int row = 0; row < mLevelSize; row ++) {
+				Tile currentTile = mLevelTiles[column][row];
+				currentTile.draw(g2d);
 			}
 		}
 	}
