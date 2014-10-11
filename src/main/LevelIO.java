35614acbec049c6d717cfcd56e974e38a4e1f6c5
KB9 <kavanbickerstaff@googlemail.com>
2014-10-11 20:26:56 +0100

    Level IO started...

---
+++ src/main/LevelIO.java
@@ -0,0 +1,18 @@
+package main;
+
+public class LevelIO {
+
+	private FileHandling mFileHandler;
+	
+	public LevelIO() {
+		mFileHandler = new FileHandling();
+	}
+	
+	public void save(Level level) {
+		 
+	}
+	
+	public Level load(String filepath) {
+		return null;
+	}
+}
